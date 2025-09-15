b704e5b5bf5bba8c48969ad02ab813550764def3
S1moT3ch <s.cantore2@studenti.poliba.it>
2025-09-15 17:59:22 +0200

    token

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -27,4 +27,22 @@ router.post('/refreshToken', authController.refreshToken);
 //logout utente
 router.post('/logout', authController.logoutUser);
 
+//login Daikin
+router.get('/auth/daikin/callback', async (req, res) => {
+    const code = req.query.code;
+    if (!code) return res.status(400).send("Missing code");
+
+    // qui scambi il code con un access_token
+    const tokenResp = await axios.post("https://idp.onecta.daikineurope.com/v1/oidc/token", qs.stringify({
+        grant_type: "authorization_code",
+        code,
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET,
+        redirect_uri: "https://acsahomeconnectv2-production.up.railway.app/"  // deve combaciare!
+    }), { headers: { "Content-Type": "application/x-www-form-urlencoded" }});
+
+    req.session.accessToken = tokenResp.data.access_token;
+    res.send("Login OK, token salvato!");
+});
+
 module.exports = router;
