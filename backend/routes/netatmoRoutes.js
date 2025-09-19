5d2a9f21ac2febb0d5d982b1412dc7b8b8781fcf
S1moT3ch <s.cantore2@studenti.poliba.it>
2025-09-19 13:08:25 +0200

    updDir

--- backend/routes/netatmoRoutes.js
+++ backend/routes/netatmoRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const netatmoController = require('../../controllers/netatmoController');
+const netatmoController = require('../controllers/netatmoController');
 
 router.get('/callback', netatmoController.getNetatmoToken)
 
